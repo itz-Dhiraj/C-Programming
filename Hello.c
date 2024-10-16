95b85e9df51162e1f2150e8cc7db8827c66dd3b9
Dhiraj <dk4009487@gmail.com>
2024-10-16 21:35:33 +0530

    commit add

--- Hello.c
+++ Hello.c
@@ -3,6 +3,7 @@ int main()
 {
     printf("Hello World");
 
+    // sum two program starts here
     int num1, num2, sum;
     printf("Enter a number for num1: ");
     scanf("%d", &num1);
@@ -12,6 +13,7 @@ int main()
     sum = num1 + num2;
 
     printf("sum is: %d", sum);
+    // sum two nums program ended here
 
     return 0;
 }
