21cfc5ec57bb3620437f45d4b476a5e3e5fbb838
Dhiraj <dk4009487@gmail.com>
2024-10-16 19:43:48 +0530

    Sum two number feature Added

--- Hello.c
+++ Hello.c
@@ -2,5 +2,16 @@
 int main()
 {
     printf("Hello World");
+
+    int num1, num2, sum;
+    printf("Enter a number for num1: ");
+    scanf("%d", &num1);
+    printf("Enter a number for num2: ");
+    scanf("%d", &num2);
+
+    sum = num1 + num2;
+
+    printf("sum is: %d", sum);
+
     return 0;
 }
