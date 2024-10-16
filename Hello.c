c76ef5ea1600edd1653cd2a874d7fd999b34bc97
Dhiraj <dk4009487@gmail.com>
2024-10-16 20:48:42 +0530

    HW

--- Hello.c
+++ Hello.c
@@ -3,15 +3,5 @@ int main()
 {
     printf("Hello World");
 
-    int num1, num2, sum;
-    printf("Enter a number for num1: ");
-    scanf("%d", &num1);
-    printf("Enter a number for num2: ");
-    scanf("%d", &num2);
-
-    sum = num1 + num2;
-
-    printf("sum is: %d", sum);
-
     return 0;
 }
